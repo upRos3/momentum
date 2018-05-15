d7755ecc8ced2d8bea700a42295b6be97ba1a69b
upRos3 <josh@upros3.com>
2018-05-15 10:43:48 -0400

    Added NavBar.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
+import render from "react-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 export default class App extends Component {
   state = {
@@ -9,6 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h1>Welcome to {this.state.name} Josh</h1>
       </div>
     );
