a14a091fba83670b1c586121fde24c910b07ec5a
upRos3 <josh@upros3.com>
2018-05-16 19:52:14 -0400

    Commented out out NavBar to ensure when future refactoring comes in, there is no confusion.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,70 +1,70 @@
-import { Component } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import LoginButton from "./LoginButton";
-
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  flex: {
-    flex: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-};
-
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  state = {
-    open: false
-  };
-
-  render() {
-    const classes = this.props;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="title"
-              color="inherit"
-              className={classes.flex}
-              style={styles.flex}
-            >
-              Momentum
-            </Typography>
-            <LoginButton />
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
-
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(NavBar);
+// import { Component } from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import LoginButton from "./LoginButton";
+//
+// import PropTypes from "prop-types";
+// import { withStyles } from "@material-ui/core/styles";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+//
+// const styles = {
+//   root: {
+//     flexGrow: 1
+//   },
+//   flex: {
+//     flex: 1
+//   },
+//   menuButton: {
+//     marginLeft: -12,
+//     marginRight: 20
+//   }
+// };
+//
+// class NavBar extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   state = {
+//     open: false
+//   };
+//
+//   render() {
+//     const classes = this.props;
+//
+//     return (
+//       <div className={classes.root}>
+//         <AppBar position="static">
+//           <Toolbar>
+//             <IconButton
+//               className={classes.menuButton}
+//               color="inherit"
+//               aria-label="Menu"
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography
+//               variant="title"
+//               color="inherit"
+//               className={classes.flex}
+//               style={styles.flex}
+//             >
+//               Momentum
+//             </Typography>
+//             <LoginButton />
+//           </Toolbar>
+//         </AppBar>
+//       </div>
+//     );
+//   }
+// }
+//
+// NavBar.propTypes = {
+//   classes: PropTypes.object.isRequired
+// };
+//
+// export default withStyles(styles)(NavBar);
